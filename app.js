53467b509bf5598af25840bcfe1ad643a9d771a5
Michael Maertens <30853323+mikemaer1990@users.noreply.github.com>
2021-04-09 14:22:30 +0800

    add pagepiling

--- app.js
+++ app.js
@@ -4,12 +4,29 @@ const check = document.querySelector('#checkbox');
 const navLink = document.querySelectorAll('.nav-link');
 
 navButton.addEventListener("click", () => {
-    check.checked ? navBar.classList.toggle("active") : navBar.classList.toggle("#nav-bar");
+    check.checked ? navBar.classList.toggle("nav-active") : navBar.classList.toggle("#nav-bar");
 })
 
 navLink.forEach(element => {
     element.addEventListener("click", () => {
-        navBar.classList.toggle("active");
+        navBar.classList.toggle("nav-active");
         check.checked = false;
     })
+});
+
+$(document).ready(function () {
+    $('#pagepiling').pagepiling({
+        verticalCentered: false,
+        direction: 'vertical',
+        anchors: ['one', 'two', 'three', 'four'],
+        menu: '.nav-links',
+        navigation: {
+            'textColor': '#f8f4ff',
+            'bulletsColor': '#f8f4ff60',
+            'position': 'right',
+            'tooltips': ['Home', 'About Me', 'Projects', 'Contact']
+        },
+        loopBottom: true,
+        loopTop: true
+    });
 });
